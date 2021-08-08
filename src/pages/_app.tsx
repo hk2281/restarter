bdecc46f708f124a34bca3d02631fcc21be36f2a
xalvaine <xalvaine@gmail.com>
2021-08-08 23:32:56 +0300

    feat: create swr global config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,32 @@ import 'src/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import { SWRConfig } from 'swr'
+import { AxiosRequestConfig } from 'axios'
 import { AuthContext, useAuthorization } from '@/authorization'
+import { api } from '@/api'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const authorization = useAuthorization()
   const router = useRouter()
 
   return (
-    <AuthContext.Provider value={{ ...authorization }}>
-      <SwitchTransition mode='out-in'>
-        <CSSTransition key={router.pathname} classNames='page' timeout={300}>
-          <div className='wrapper'>
-            <Component {...pageProps} />
-          </div>
-        </CSSTransition>
-      </SwitchTransition>
-    </AuthContext.Provider>
+    <SWRConfig
+      value={{
+        fetcher: (url: string, config?: AxiosRequestConfig) =>
+          api.get(url, config).then((response) => response.data),
+      }}
+    >
+      <AuthContext.Provider value={{ ...authorization }}>
+        <SwitchTransition mode='out-in'>
+          <CSSTransition key={router.pathname} classNames='page' timeout={300}>
+            <div className='wrapper'>
+              <Component {...pageProps} />
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
+      </AuthContext.Provider>
+    </SWRConfig>
   )
 }
 
--- src/screens/Containers/Containers.tsx
+++ src/screens/Containers/Containers.tsx
@@ -1,11 +1,7 @@
 import useSWR from 'swr'
-import { api } from '@/api'
 
 export const Containers = () => {
-  const { data: containers } = useSWR(`/containers`, api.get, {
-    revalidateOnFocus: false,
-    shouldRetryOnError: false,
-  })
+  const { data: containers } = useSWR(`/containers`)
 
   console.log(containers)
 
