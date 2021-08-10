aaecc32be9f9b85c10342ece1e1af57af35cdf50
xalvaine <xalvaine@gmail.com>
2021-08-11 01:20:02 +0300

    feat: improve login page

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,7 +1,5 @@
 import { Button, Form, Input, Typography } from 'antd'
-import Link from 'next/link'
 import { UserOutlined } from '@ant-design/icons'
-import { PATH } from '@/config'
 import { useSubmission } from '@/screens/Login/hooks/useSubmission'
 import { OneAuthorizationStateRoute } from '@/utils/authorization'
 import { UnauthorizedLayout } from '@/shared/UnauthorizedLayout'
@@ -14,18 +12,19 @@ export const Login = () => {
       <Typography.Title>Вход</Typography.Title>
       <Form onFinish={handleSubmit}>
         <Form.Item name='email'>
-          <Input style={{ color: `#00000073` }} suffix={<UserOutlined />} />
+          <Input
+            size='large'
+            style={{ color: `#00000073` }}
+            suffix={<UserOutlined />}
+          />
         </Form.Item>
         <Form.Item name='password'>
-          <Input.Password />
+          <Input.Password size='large' />
         </Form.Item>
         <Form.Item>
-          <Button htmlType='submit'>Вход</Button>
-        </Form.Item>
-        <Form.Item>
-          <Link passHref href={PATH.SIGNUP}>
-            <Button htmlType='submit'>Регистрация</Button>
-          </Link>
+          <Button block htmlType='submit' size='large' type='primary'>
+            Войти
+          </Button>
         </Form.Item>
       </Form>
     </OneAuthorizationStateRoute>
