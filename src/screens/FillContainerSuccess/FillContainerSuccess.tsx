00c066114875487eef5d603d21cad42ce8831f30
xalvaine <xalvaine@gmail.com>
2021-08-11 20:21:11 +0300

    feat: add container filling success result

--- src/screens/FillContainerSuccess/FillContainerSuccess.tsx
+++ src/screens/FillContainerSuccess/FillContainerSuccess.tsx
@@ -1,7 +1,32 @@
+import { Button, Result, Typography } from 'antd'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 import { UnauthorizedLayout } from '@/shared/UnauthorizedLayout'
+import { PATH } from '@/config'
 
 export const FillContainerSuccess = () => {
-  return <>aklsdma</>
+  const router = useRouter()
+
+  return (
+    <>
+      <Head>
+        <title>Контейнер {router.query.container} заполнен</title>
+      </Head>
+      <Typography.Paragraph>Контейнер заполнен</Typography.Paragraph>
+      <Typography.Title level={4}>ID {router.query.container}</Typography.Title>
+      <Result
+        extra={[
+          <Link key='homeLink' passHref href={PATH.HOME}>
+            <Button type='primary'>Окей</Button>
+          </Link>,
+        ]}
+        status='success'
+        subTitle='В день вывоза специально обученные люди заберут макулатуру прямо из офиса.'
+        title='Вы молодец!'
+      />
+    </>
+  )
 }
 
 FillContainerSuccess.layout = UnauthorizedLayout
