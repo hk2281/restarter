4abb483e657ac683d7a0fd34f8942a54e150e908
xalvaine <xalvaine@gmail.com>
2021-08-17 23:01:44 +0300

    feat: add useMediaQuery hook

---
+++ src/utils/useMediaQuery.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+export const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false)
+  const [rendered, setRendered] = useState(false)
+  const [calculated, setCalculated] = useState(false)
+
+  useEffect(() => setRendered(true), [])
+
+  useEffect(() => {
+    if (!rendered) return
+    const media = window.matchMedia(query)
+    const listener = () => {
+      setMatches(media.matches)
+    }
+    media.addEventListener(`change`, listener, { once: true })
+    listener()
+    setCalculated(true)
+    return () => media.removeEventListener(`change`, listener)
+  }, [query, rendered])
+
+  return { matches, rendered: rendered && calculated }
+}
