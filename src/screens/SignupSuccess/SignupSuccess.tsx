c58fa789d99f879383abb830bd64aa5773b107b4
xalvaine <xalvaine@gmail.com>
2021-08-20 00:56:13 +0300

    feat: add registration success screen

--- src/screens/SignupSuccess/SignupSuccess.tsx
+++ src/screens/SignupSuccess/SignupSuccess.tsx
@@ -1,3 +1,28 @@
+import Head from 'next/head'
+import { Button, Result, Typography } from 'antd'
+import Link from 'next/link'
+import { PATH } from '@/config'
+import { UnauthorizedLayout } from '@/shared/components/UnauthorizedLayout'
+
 export const SignupSuccess = () => {
-  return <></>
+  return (
+    <>
+      <Head>
+        <title>Вы молодец!</title>
+      </Head>
+      <Typography.Title level={4}>Поздравляем!</Typography.Title>
+      <Result
+        extra={[
+          <Link key='homeLink' passHref href={PATH.HOME}>
+            <Button type='primary'>Окей</Button>
+          </Link>,
+        ]}
+        status='success'
+        subTitle='Проверьте вашу почту. Там вы найдете дальнейшую информацию по подключению. Спасибо!'
+        title='Ещё чуть-чуть'
+      />
+    </>
+  )
 }
+
+SignupSuccess.layout = UnauthorizedLayout
