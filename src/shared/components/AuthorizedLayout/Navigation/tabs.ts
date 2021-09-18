27d9f9689eea05cf287754d668cd72ecc9c1951c
xalvaine <xalvaine@gmail.com>
2021-09-19 00:23:18 +0300

    chore: remove tokens page

--- src/shared/components/AuthorizedLayout/Navigation/tabs.ts
+++ src/shared/components/AuthorizedLayout/Navigation/tabs.ts
@@ -4,5 +4,5 @@ export const tabs = [
   { path: PATH.CONTAINERS, title: `Контейнеры` },
   { path: PATH.EVENTS, title: `События` },
   { path: PATH.SETTINGS, title: `Настройки` },
-  { path: PATH.TOKENS, title: `Токены` },
+  // { path: PATH.TOKENS, title: `Токены` },
 ]
