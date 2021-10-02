a76df053a3446f6520c9a2e7ecfe301a410045b5
xalvaine <xalvaine@gmail.com>
2021-10-02 21:57:09 +0300

    feat: Добавить сбор архива

--- src/screens/Events/Gatherings/Gatherings.tsx
+++ src/screens/Events/Gatherings/Gatherings.tsx
@@ -1,26 +1,48 @@
-import { Card, Typography } from 'antd'
+import { Button, Card, Form, Input, Modal, Select, Typography } from 'antd'
 import useSWR from 'swr'
 import moment from 'moment'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import classNames from 'classnames'
 import styles from 'src/screens/Events/Gatherings/Gatherings.module.scss'
 import { GatheringModal } from '@/screens/Events/Gatherings/GatheringModal/GatheringModal'
+import { useBuildings } from '@/shared/hooks/use-buildings'
+import { api } from '@/api'
 
 const getStatus = (gathering: Backend.Gathering) => {
   return gathering.confirmed_at ? `выполнен` : `надо выполнить`
 }
 
 export const Gatherings = () => {
-  const { data: gatherings } = useSWR<Backend.Gathering[]>(
+  const { data: gatherings, mutate } = useSWR<Backend.Gathering[]>(
     `/container-takeout-requests`,
   )
   const [id, setId] = useState<number | undefined>()
+  const [visible, setVisible] = useState(false)
+  const [form] = Form.useForm()
+  const { buildings } = useBuildings()
+
+  const handleAddExporting = useCallback(
+    async (values: Record<string, unknown>) => {
+      await api.post(`/container-takeout-requests`, values)
+      await mutate()
+      setVisible(false)
+    },
+    [mutate],
+  )
 
   return (
     <div className={styles.wrapper}>
       <Typography.Title className={styles.title} level={2}>
         Сборы
       </Typography.Title>
+      <Button
+        block
+        className={styles.button}
+        size='large'
+        onClick={() => setVisible(true)}
+      >
+        Организовать сбор архива
+      </Button>
       {gatherings
         ?.slice()
         .reverse()
@@ -50,6 +72,43 @@ export const Gatherings = () => {
           </Card>
         ))}
       <GatheringModal close={() => setId(undefined)} id={id} />
+      <Modal
+        footer={[
+          <Button
+            key='create'
+            size='large'
+            type='primary'
+            onClick={form.submit}
+          >
+            Создать
+          </Button>,
+        ]}
+        title='Создать вызов'
+        visible={visible}
+        onCancel={() => setVisible(false)}
+      >
+        <Form className={styles.form} form={form} onFinish={handleAddExporting}>
+          <Form.Item name='requesting_worker_name'>
+            <Input placeholder='ФИО обратившегося сотрудника' size='large' />
+          </Form.Item>
+          <Form.Item name='requesting_worker_phone'>
+            <Input placeholder='Телефон сотрудника' size='large' />
+          </Form.Item>
+          <Form.Item name='building'>
+            <Select
+              options={buildings}
+              placeholder='Выберите здание'
+              size='large'
+            />
+          </Form.Item>
+          <Form.Item name='archive_room'>
+            <Input placeholder='Аудитория' size='large' />
+          </Form.Item>
+          <Form.Item name='archive_mass'>
+            <Input placeholder='Объем сбора' size='large' />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   )
 }
